d4f9de70e2f5bd5316c4079352d660f5dfe11291
Oleg Kovalov <iamolegkovalov@gmail.com>
2018-11-24 17:10:02 +0100

    fix examples

--- tsvreader_example_test.go
+++ tsvreader_example_test.go
@@ -1,17 +1,40 @@
-package tsvreader_test
+package dsvreader_test
 
 import (
 	"bytes"
 	"fmt"
-	"github.com/valyala/tsvreader"
+
+	"github.com/cristaloleg/dsvreader"
 )
 
 func ExampleReader() {
 	bs := bytes.NewBufferString(
-		"foo\t42\n" +
-			"bar\t123\n")
+		`foo	42
+bar	123
+`)
 
-	r := tsvreader.New(bs)
+	r := dsvreader.NewTSV(bs)
+	for r.Next() {
+		col1 := r.String()
+		col2 := r.Int()
+		fmt.Printf("col1=%s, col2=%d\n", col1, col2)
+	}
+	if err := r.Error(); err != nil {
+		fmt.Printf("unexpected error: %s", err)
+	}
+
+	// Output:
+	// col1=foo, col2=42
+	// col1=bar, col2=123
+}
+
+func ExampleCSVReader() {
+	bs := bytes.NewBufferString(
+		`foo,42
+bar,223
+`)
+
+	r := dsvreader.NewCSV(bs)
 	for r.Next() {
 		col1 := r.String()
 		col2 := r.Int()
@@ -33,7 +56,7 @@ func ExampleReader_HasCols() {
 			"\n" +
 			"a\tb\tc\n")
 
-	r := tsvreader.New(bs)
+	r := dsvreader.NewTSV(bs)
 	for r.Next() {
 		for r.HasCols() {
 			s := r.String()
@@ -55,7 +78,7 @@ func ExampleReader_HasCols() {
 func ExampleReader_Next() {
 	bs := bytes.NewBufferString("1\n2\n3\n42\n")
 
-	r := tsvreader.New(bs)
+	r := dsvreader.NewTSV(bs)
 	for r.Next() {
 		n := r.Int()
 		fmt.Printf("%d\n", n)
